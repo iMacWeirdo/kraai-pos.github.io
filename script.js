bcaf43c3e1610d064ea06fcc1241bf10b359a666
Wyatt Kraai <75582820+iMacWeirdo@users.noreply.github.com>
2025-05-04 15:15:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('loginForm').onsubmit = async function(event) {
     const username = this[0].value;
     const password = this[1].value;
 
-    const response = await fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbzN_eTpTZNbfdkleUqx1zQYbhCurCVE7a7mdkJ-0LJVceeWxw6__mWhO9HGhSHVuJ8/exec', {
         method: 'POST',
         body: JSON.stringify({ action: 'login', username, password }),
         headers: { 'Content-Type': 'application/json' }
